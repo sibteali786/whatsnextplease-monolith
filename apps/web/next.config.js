bd7efa8bc15f553e8a24f1454badc948e9284c91
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-01-10 12:14:15 +0500

    fix: 3

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,32 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	transpilePackages: ["geist", "lucide-react"],
-	images: {
-	  remotePatterns: [
-		{
-		  protocol: "https",
-		  hostname: "avatars.githubusercontent.com",
-		  port: "",
-		  pathname: "/u/**",
-		},
-	  ],
-	},
-	webpack: (config, { isServer }) => {
-	  if (isServer) {
-		// Handle server-side modules
-		config.externals = [...config.externals, 
-		  'thread-stream'
-		];
-	  }
-	  
-	  // Add path alias
-	  config.resolve.alias = {
-		...config.resolve.alias,
-		'@': '.',
-	  };
-  
-	  return config;
-	},
-  };
-  
-  export default nextConfig;
+  experimental: {
+    serverComponentsExternalPackages: [
+      "thread-stream",
+      "pino",
+      "pino-worker",
+      "pino-file",
+      "pino-pretty",
+    ],
+  },
+  transpilePackages: ["geist", "lucide-react"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    // Add path alias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": ".",
+    };
+
+    return config;
+  },
+};
+
+export default nextConfig;
