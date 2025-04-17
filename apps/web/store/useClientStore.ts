c8b997d45d36947f9b0fc9dbf3c60fff0442df1e
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-04-17 13:24:30 +0500

    feat: refactor client store to include logged-in client state and persist functionality

--- apps/web/store/useClientStore.ts
+++ apps/web/store/useClientStore.ts
@@ -1,12 +1,39 @@
-import { create } from "zustand";
-interface ClientState {
+import { Client, Roles } from '@prisma/client';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+interface SelectedClientState {
   selectedClient: { id: string; username: string } | null;
   setSelectedClient: (client: { id: string; username: string }) => void;
   clearSelectedClient: () => void;
 }
 
-export const useClientStore = create<ClientState>((set) => ({
+export const useClientStore = create<SelectedClientState>(set => ({
   selectedClient: null,
-  setSelectedClient: (client) => set({ selectedClient: client }),
+  setSelectedClient: client => set({ selectedClient: client }),
   clearSelectedClient: () => set({ selectedClient: null }),
 }));
+
+type LoggedInClient = Omit<Client, 'passwordHash'> & {
+  role: {
+    name: Roles | undefined;
+  } | null;
+};
+
+interface LoggedInClientState {
+  client: LoggedInClient | null;
+  setClient: (client: LoggedInClient) => void;
+  clearClient: () => void;
+}
+
+export const useLoggedInClientState = create<LoggedInClientState>()(
+  persist(
+    set => ({
+      client: null,
+      setClient: client => set({ client }),
+      clearClient: () => set({ client: null }),
+    }),
+    {
+      name: 'logged-in-client-store', // Key in localStorage
+    }
+  )
+);
