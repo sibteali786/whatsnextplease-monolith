4c003bbadb7fba2432f5d8544091ebf7e9bf02fa
MuhammadSanaullah123 <muhammed14336@gmail.com>
2025-11-20 19:01:46 +0300

    fix: integrated functionality of client task offerings

--- apps/web/components/clients/ClientTaskList.tsx
+++ apps/web/components/clients/ClientTaskList.tsx
@@ -5,10 +5,15 @@ import { UserTasksTable } from '../users/UserTaskTable';
 import { getTasksByClientId } from '@/db/repositories/clients/getTasksByClientId';
 import { getTaskIdsByClientId } from '@/utils/clientActions';
 import { SearchNFilter } from '../common/SearchNFilter';
-import { Roles } from '@prisma/client';
+import { Roles, TaskPriorityEnum, TaskStatusEnum } from '@prisma/client';
 import { TaskTable } from '@/utils/validationSchemas';
+import { DurationEnum, DurationEnumList } from '@/types';
+import { transformEnumValue } from '@/utils/utils';
+import { useSearchParams } from 'next/navigation';
 
-export default function ClientTaskList({ clientId }: { clientId: string }) {
+export default function ClientTaskList({ clientId, role }: { clientId: string; role: Roles }) {
+  const searchParams = useSearchParams();
+
   const [data, setData] = useState<TaskTable[]>([]);
   const [cursor, setCursor] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
@@ -17,16 +22,60 @@ export default function ClientTaskList({ clientId }: { clientId: string }) {
   const [pageIndex, setPageIndex] = useState(0);
   const [taskIds, setTaskIds] = useState<string[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
-
+  const [duration, setDuration] = useState<DurationEnum>(DurationEnum.ALL);
+  const listToFilterUpon: DurationEnumList = Object.values(DurationEnum).map(duration => ({
+    label: duration,
+    value: transformEnumValue(duration),
+  }));
+  const statusFilter = searchParams.get('status');
+  const priorityFilter = searchParams.get('priority');
+  const typeFilter: 'all' | 'assigned' | 'unassigned' | 'my-tasks' = searchParams.get('type') as
+    | 'all'
+    | 'assigned'
+    | 'unassigned'
+    | 'my-tasks';
   const fetchTasks = async () => {
     setLoading(true);
     try {
-      const response = await getTasksByClientId(clientId, cursor, pageSize);
-      const responseIds = await getTaskIdsByClientId(clientId);
+      // Split the comma-separated status values
+      const statusArray = statusFilter?.split(',') || [];
+      const priorityArray = priorityFilter?.split(',') || [];
+
+      // If there are status values, map them to TaskStatusEnum
+      const normalizedStatus = statusArray
+        ? statusArray
+            .map((status: string) => TaskStatusEnum[status as keyof typeof TaskStatusEnum] || null)
+            .filter(status => status !== null)
+        : [];
+      const normalizedPriority = priorityArray
+        ? priorityArray
+            .map(
+              (priority: string) =>
+                TaskPriorityEnum[priority as keyof typeof TaskPriorityEnum] || null
+            )
+            .filter(priority => priority !== null)
+        : [];
+      const response = await getTasksByClientId(
+        typeFilter ?? 'unassigned',
+
+        searchTerm,
+        duration,
+        clientId,
+        cursor,
+        pageSize,
+        normalizedStatus,
+        normalizedPriority
+      );
+      const responseIds = await getTaskIdsByClientId(
+        typeFilter ?? 'unassigned',
+        searchTerm,
+        duration,
+        clientId
+      );
 
       if (response.success && responseIds.success && response.tasks) {
-        const filteredTasks = response.tasks.filter(task => task.status.statusName !== 'COMPLETED');
-        setData(filteredTasks);
+        /*         const filteredTasks = response.tasks.filter(task => task.status.statusName !== 'COMPLETED'); */
+        setData(response.tasks || []);
         if (response.totalCount) {
           setTotalCount(response.totalCount);
         }
@@ -40,11 +89,14 @@ export default function ClientTaskList({ clientId }: { clientId: string }) {
 
   useEffect(() => {
     fetchTasks();
-  }, [cursor, pageSize, searchTerm]);
-
+  }, [cursor, pageSize, searchTerm, duration, statusFilter, priorityFilter, typeFilter]);
+  const handleSearch = (term: string, duration: DurationEnum) => {
+    setSearchTerm(term); // Update the search term when user searches
+    setDuration(duration);
+  };
   return (
     <div className="flex flex-col gap-4">
-      <SearchNFilter onSearch={term => setSearchTerm(term)} />
+      <SearchNFilter onSearch={handleSearch} filterList={listToFilterUpon} role={Roles.CLIENT} />
       <UserTasksTable
         data={data}
         pageSize={pageSize}
@@ -57,8 +109,9 @@ export default function ClientTaskList({ clientId }: { clientId: string }) {
         showDescription={true}
         setPageSize={setPageSize}
         taskIds={taskIds}
-        role={Roles.CLIENT}
+        role={role}
         showAsModal={true}
+        fetchTasks={fetchTasks}
       />
     </div>
   );
